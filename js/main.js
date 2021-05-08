8f20e19c82e5f2dafcb932367708dc10086c947c
John Gouk <johngouk@googlemail.com>
2021-05-08 14:46:38 +0100

    Update main.js
    Added error() function and timeout for operation, also logging all values from getCurrentPosition().

--- js/main.js
+++ js/main.js
@@ -1,16 +1,58 @@
-<script>
-$.(document).ready(function () {
-  var x = document.getElementById("demo");
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(showPosition);
-    } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-  }
-  function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
-  }
+function showPosition(position) {
+  clearTimeout(getLocationTimeout);
+  getLocationTimeout = 0;
+  $('#demo').html("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude +
+    "<br>Accuracy: " + position.coords.accuracy +
+    "<br>Altitude: " + position.coords.altitude +
+    "<br>AtlAccuracy: " + position.coords.altitudeAccuracy +
+    "<br>Heading: " + position.coords.heading +
+    "<br>Speed: " + position.coords.speed +
+    "<br>Timestamp: " + position.timestamp);
 }
-</script>
+
+function errorPosition(error){
+  clearTimeout(getLocationTimeout);
+  getLocationTimeout = 0;
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        $('#demo').text("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        $('#demo').text("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        $('#demo').text("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        $('#demo').text("An unknown error occurred.");
+        break;
+    }
+}
+
+function getLocationFailed (){
+  $('#demo').text("getCurrentPosition timed out!");
+}
+
+let getLocationTimeout = 0;
+
+$(document).ready(function() {
+      console.log("Document ready!");
+//      $('#demo').text("Geolocation is not supported by this browser.");
+
+      $('#getLocation').click(function() {
+        console.log("location requested");
+          
+          if (navigator.geolocation) {
+            if (getLocationTimeout != 0) {
+              clearTimeout(getLocationTimeout);
+              getLocationTimeout = 0;
+            }
+            getLocationTimeout = setTimeout(getLocationFailed, 100000);
+            navigator.geolocation.getCurrentPosition(showPosition, errorPosition, {timeout:90000});
+          } else {
+            $('#demo').text("Geolocation is not supported by this browser.");
+          }
+      });
+
+});
