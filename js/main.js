a79904c1edd97cd5f202b37d8888d2982170ad75
John Gouk <johngouk@googlemail.com>
2021-05-08 23:18:46 +0100

    Added code for Echo URL handling
    Fixed:
    - escaping of embedded URL
    - typo in location info

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+var getParams = {maximumAge:60000, timeout:5000, enableHighAccuracy:true}; // Not currently used
+var weatherURL = "https://dataservice.accuweather.com/locations/v1/cities/search?apikey=Kh2fOXMXIZS1geBmXqKnstfbbmOpW8up&q=RG306AF";
+var serverURL = "https://server.local/cgi-bin/test.py?url=";
+var echoURL = "https://server.local/cgi-bin/echo.py?url=";
+
 function showPosition(position) {
   clearTimeout(getLocationTimeout);
   getLocationTimeout = 0;
@@ -5,7 +10,7 @@ function showPosition(position) {
     "<br>Longitude: " + position.coords.longitude +
     "<br>Accuracy: " + position.coords.accuracy +
     "<br>Altitude: " + position.coords.altitude +
-    "<br>AtlAccuracy: " + position.coords.altitudeAccuracy +
+    "<br>AltAccuracy: " + position.coords.altitudeAccuracy +
     "<br>Heading: " + position.coords.heading +
     "<br>Speed: " + position.coords.speed +
     "<br>Timestamp: " + position.timestamp);
@@ -34,6 +39,32 @@ function getLocationFailed (){
   $('#demo').text("getCurrentPosition timed out!");
 }
 
+function getWeatherError(xhr, statusCode, error) {
+  console.log("gWE:"+statusCode+error);
+}
+
+function getWeatherOk(result, statusCode, xhr){
+  console.log("gWO:status:"+statusCode+":result:"+result[0]);
+  if (statusCode == "success"){
+//    console.log("gWO: values:"+Object.values(result[0]));
+//    var resultJSON = JSON.parse(result);
+    console.log ("Result:\n" + Object.entries(result[0]));
+  } else {
+    console.log("status:"+statusCode);
+  }
+}
+
+function getEchoOk(result, statusCode, xhr){
+  console.log("gEO:status:"+statusCode+":result:"+result);
+  if (statusCode == "success"){
+    console.log("gEO: values:"+Object.values(result[0]));
+    $("#demo").html(result);
+  } else {
+    console.log("status:"+statusCode);
+  }
+}
+
+
 let getLocationTimeout = 0;
 
 $(document).ready(function() {
@@ -54,5 +85,19 @@ $(document).ready(function() {
             $('#demo').text("Geolocation is not supported by this browser.");
           }
       });
+
+      $('#getWeather').click(function() {
+        console.log("URL: "+serverURL+escape(weatherURL));
+        $.getJSON(serverURL+escape(weatherURL), getWeatherOk);
+//        $.get(serverURL+escape(weatherURL), getWeatherOk);
+        console.log("Sent request");
+      });
+
+      $('#getEcho').click(function() {
+        console.log("URL: "+echoURL+escape(weatherURL));
+        $.get(echoURL+escape(weatherURL), getEchoOk);
+        console.log("Sent request");
+      });
+
 
 });
